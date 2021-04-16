a3b6fed4416b8a8ac1bebb921960509a4b41ac6c
kotaro1574 <shake1574@gmail.com>
2021-04-16 11:45:08 +0900

    api

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const SWindex = () => {
-  // const [SW, setSW] = useState([])
-  // getData(() => {
-  //   axios.get(`https://swapi.dev/api/people`)
-  //     .then(response => setSW(response.data))
-  //     .catch(error => console.log(error))
-  // }, [])
-
-
-  const getData = async () => {
-        try {    
-          const result = await axios.get('/api/people/')
-          console.log(result);
-        } catch (error) {    
-          console.log("error!!");
-        }
-    };
+  const [SW, setSW] = useState([])
+  const getData = () => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then((response) => {
+        console.log(response.data.results)
+        // setSW(response.data.results.map(name => name.name))
+      })
+      .catch(error => console.log(error))
+  }
 
   return (
     <>
       <div className="App">
-      <div>
-        <button onClick={() =>getData()}>get profile!</button>
+        <div>
+          <button onClick={getData()}>get profile!</button>
+          {SW}
+        </div>
       </div>
-    </div>
       {/* <ul>
-        { SW.map(SW => <li>{SW.results}</li>)}
+        { SW.map(SW => <li>{SW}</li>)}
       </ul> */}
     </>
   );
--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-import proxy from 'http-proxy-middleware';
- 
-module.exports = function(app) {
-    const headers  = {
-        "Content-Type": "application/json",
-    }
-    app.use(proxy("/api/people/", { target: "https://swapi.dev/",changeOrigin: true,secure: false,headers: headers}));  
-};
